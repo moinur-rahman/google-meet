c0f41d5947a3c8041eb8147f3978b6d7d61eda0f
moinur-rahman <rahmanmoin27@gmail.com>
2021-12-31 20:46:54 +0600

    2nd commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "")));
+
+server.listen(port, () => {
+  console.log("Listening to port ", port);
+});
+
+userConnections = [];
+
+io.on("connection", (socket) => {
+  console.log("Socket id is ", socket.id);
+  socket.on("userconnect", (data) => {
+    console.log("userconnect ", data.displayName, data.meetingid);
+    console.log(userConnections);
+    var other_users = userConnections.filter(
+      (p) => p.meeting_id == data.meetingid
+    );
+    userConnections.push({
+      connectionId: socket.id,
+      user_id: data.displayName,
+      meeting_id: data.meetingid,
+    });
+
+    other_users.forEach((v) => {
+      socket.to(v.connectionId).emit("inform_others_about_me", {
+        other_user_id: data.displayName,
+        connId: socket.id,
+      });
+    });
+     socket.emit("inform_me_about_other_user",other_users)
+  });
+  socket.on("SDPProcess", (data) => {
+    socket.to(data.to_connid).emit("SDPProcess", {
+      message: data.message,
+      from_connid: socket.id,
+    });
+  });
+});
